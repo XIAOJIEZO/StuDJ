1e2663e1f90d42f204c907bf3dfa285d2aee01f6
LIJ <lij@LIJdeMacBook-Pro.local>
2021-12-27 12:03:05 +0800

    updata

--- testP/api/generate_user_information.py
+++ testP/api/generate_user_information.py
@@ -4,9 +4,8 @@ from utils import fake
 
 
 def generate_user_information(request):
-    user = fake.Users().generate_users()
-
     if request.POST:
+        user = fake.Users().generate_users()
         return render(request, "generate_user_information.html", {"user_dict": user})
 
     else:
