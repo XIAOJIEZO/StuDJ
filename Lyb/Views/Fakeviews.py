cba263cb51bf25f90dc1130e6c87df58267cbcf6
2601804605@qq.com <Liujie26018>
2022-06-28 14:52:37 +0800

    添加swagger

--- Lyb/Views/Fakeviews.py
+++ Lyb/Views/Fakeviews.py
@@ -1,4 +1,6 @@
 from django.views.decorators.csrf import csrf_exempt
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,7 +8,12 @@ from Lyb.Lybmodels import fakeobj
 from Lyb.Serializer.FakeSerializer import FakeSerializer
 from utils.apiresponse import APIResponse
 
+request_body = openapi.Schema(type=openapi.TYPE_OBJECT,
+                              required=['locale'],
+                              properties={'locale': openapi.Schema(type=openapi.TYPE_STRING, description='zh_CN zh_TW en_US ja_JP')})
 
+
+@swagger_auto_schema(method='post', request_body=request_body)
 @csrf_exempt
 @api_view(['POST'])
 def fakeinfo(request):
